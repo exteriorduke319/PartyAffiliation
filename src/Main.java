e92bd39196ff320416828ecd5ca2455e1778972b
exteriorduke319 <yarnevictanner@gmail.com>
2023-09-28 09:20:56 -0400

    init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        String party;
+
+        System.out.print("Please enter your party affiliation(D, R, I, or something else): ");
+        party = scan.nextLine();
+
+        if (Objects.equals(party, "D")) {
+            System.out.println("You get a democratic donkey");
+        } else if (Objects.equals(party, "R")) {
+            System.out.println("You get a republican rhino");
+        } else {
+            System.out.println("You get an independent man");
+        }
     }
 }
